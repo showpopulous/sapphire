e7aa5e508c405372074ae67a3c8747be173137f0
zettafi <zettafi@localhost.localdomain>
2022-10-11 13:06:45 -0400

    implemented blue star

---
+++ Rakefile
@@ -0,0 +1,5 @@
+task :default => :spec
+
+task :spec do
+  sh "rspec *_spec.rb"
+end
---
+++ update_quality.rb
@@ -0,0 +1,54 @@
+require 'award'
+
+def update_quality(awards)
+  awards.each do |award|
+    if award.name == 'Blue Star'
+      if award.quality > 0 
+        quality_degrade = award.expires_in > 0 ? 2 : 4
+        award.quality -= quality_degrade
+      end
+    elsif award.name != 'Blue First' && award.name != 'Blue Compare'
+      if award.quality > 0
+        if award.name != 'Blue Distinction Plus'
+          award.quality -= 1
+        end
+      end
+    else
+      if award.quality < 50
+        award.quality += 1
+        if award.name == 'Blue Compare'
+          if award.expires_in < 11
+            if award.quality < 50
+              award.quality += 1
+            end
+          end
+          if award.expires_in < 6
+            if award.quality < 50
+              award.quality += 1
+            end
+          end
+        end
+      end
+    end
+    if award.name != 'Blue Distinction Plus'
+      award.expires_in -= 1
+    end
+    if award.expires_in < 0
+      if award.name != 'Blue First'
+        if award.name != 'Blue Compare'
+          if award.quality > 0
+            if !['Blue Distinction Plus', 'Blue Star'].include? award.name
+              award.quality -= 1
+            end
+          end
+        else
+          award.quality = award.quality - award.quality
+        end
+      else
+        if award.quality < 50
+          award.quality += 1
+        end
+      end
+    end
+  end
+end
